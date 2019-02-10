52c3a3e58551fde2b049347bc84c5e83b4ef230c
kry4a <nikita.kruchinkin@gmail.com>
2019-02-10 14:02:13 +0300

    Add parallax

---
+++ docs/js/bundle.js
@@ -0,0 +1,2 @@
+
+$(function(){console.log('init');var w_width=$(window).width();var target=$(this).data('target');if(w_width>767){$('.parallax').parallax({mouseport:$("body"),xorigin:0,yorigin:0},{xparallax:'50px',yparallax:'50px'});}});
